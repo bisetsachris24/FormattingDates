5bd863616fee901e68520628f68faa63c857cbed
amanichristianbisetsa <bisetsachristian@gmail.com>
2026-04-21 15:51:30 -0500

    commit

--- src/main/java/com/pluralsight/FormatDates.java
+++ src/main/java/com/pluralsight/FormatDates.java
@@ -9,27 +9,28 @@ public class FormatDates    {
     public static void main(String[] args) {
 
         // Get current local date/time
-        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime mylocation= LocalDateTime.now();
 
         // Creating formater 1
-        DateTimeFormatter format1 = DateTimeFormatter.ofPattern("MM/dd/yyyy");
-        System.out.println(now.format(format1));
+        DateTimeFormatter format1 = DateTimeFormatter.ofPattern(" MM/dd/yyyy");
+        System.out.println(mylocation.format(format1));
 
         // Creating formater 2
         DateTimeFormatter format2 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        System.out.println(now.format(format2));
+        System.out.println(mylocation.format(format2));
 
         //Creating formater 3
         DateTimeFormatter format3 = DateTimeFormatter.ofPattern("EEEE dd, yyyy");
-        System.out.println(now.format(format3));
+        System.out.println(mylocation.format(format3));
 
         // GMT Time
         ZonedDateTime gmtTime = ZonedDateTime.now(ZoneId.of("GMT"));
-        DateTimeFormatter format4 = DateTimeFormatter.ofPattern("EEEE, MMM dd, yyyy HH:mm");
-        System.out.println(gmtTime.format(format4));
+
+        DateTimeFormatter gmtFormat = DateTimeFormatter.ofPattern("EEEE, MMM dd, yyyy HH:mm");
+        System.out.println(gmtTime.format(gmtFormat));
 
         // Bonus
         DateTimeFormatter format5 = DateTimeFormatter.ofPattern("H:mm 'on' dd-MMM-yyyy");
-        System.out.println(now.format(format5));
+        System.out.println(mylocation.format(format5));
     }
 }
